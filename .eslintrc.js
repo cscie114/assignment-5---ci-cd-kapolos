f11197b3f425dde3971bda52401a88d60e352f16
dev <dev@linksalvation.com>
2023-04-23 16:03:53 +0300

    Added testing framework & created tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: ['./node_modules/ts-standardx/.eslintrc.js'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx', '*.ts'],
